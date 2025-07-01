6da678ed00addbb2c4fc5838cb2c9fffea737007
mukha_0 <muhammadali8261@mail.ru>
2025-07-01 19:00:51 +0500

    Added PLUS method

---
+++ MathCalc/Math/Calculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalc.Math
+{
+    internal class Calculations
+    {
+        public void Plus(int a, int b)
+        {
+            Console.WriteLine($"The result of {a} + {b} is {a + b}");
+        }
+    }
+}
